5dced9f1017a02c5ed57755631b22c556d792004
Rex Chiu <rex@iotum.com>
2025-08-12 19:11:58 -0400

    fix package removal

--- scripts/ios/before_plugin_uninstall.js
+++ scripts/ios/before_plugin_uninstall.js
@@ -1,6 +1,8 @@
 var helper = require("./helper");
+const { setContext } = require('../lib/utilities');
 
 module.exports = function(context) {
+    setContext(context);
 
     // Remove the build script that was added when the plugin was installed.
     var xcodeProjectPath = helper.getXcodeProjectPath();
